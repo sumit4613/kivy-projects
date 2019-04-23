116e0e407d2abc691f36072a65ed7ea0d3f85553
Sumit Singh <sumiterw11@gmail.com>
2019-04-23 22:21:24 +0530

    added few lines of code

--- firstapp.py
+++ firstapp.py
@@ -1,13 +1,39 @@
 import kivy
 from kivy.app import App
 
-from kivy.uix.label import Label  #For text
+from kivy.uix.label import Label  #For rendering text
+from kivy.uix.gridlayout import GridLayout  # widgets
+from kivy.uix.textinput import TextInput  #input text from user 
 
 kivy.require("1.10.1")
 
-class MyApp(App):
+class ConnectPage(GridLayout):
+	"""docstring for ConnectPage"""
+	def __init__(self, **kwargs):
+		super().__init__(**kwargs)
+		self.cols = 2
+
+		self.add_widget(Label(text="IP:"))
+
+		self.ip = TextInput(multiline=False)
+		self.add_widget(self.ip)
+
+		self.add_widget(Label(text="Port:"))
+
+		self.port = TextInput(multiline=False)
+		self.add_widget(self.port)
+		
+		self.add_widget(Label(text="Username:"))
+
+		self.username = TextInput(multiline=False)
+		self.add_widget(self.username)
+		
+		
+
+
+class DemoApp(App):
 	def build(self):
-		return Label(text="Hello There!")
+		return ConnectPage()
 
 if __name__ == '__main__':
-	MyApp().run()
+	DemoApp().run()
