068f4fbd28d90c95e8ebf800203684165431e912
Sumit Singh <sumiterw11@gmail.com>
2019-04-25 21:26:57 +0530

    playing with sockets and addded requirements.txt

--- python-socket/server.py
+++ python-socket/server.py
@@ -2,6 +2,8 @@
 
 import socket
 
+HEADERSIZE = 10
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 """ AF_INET corresponds to IPV4
 	SOCK_STREAM corresponds for TCP """
@@ -12,5 +14,8 @@ s.listen(5)
 while True:
 	clientsocket, address = s.accept()
 	print(f"Connection from {address} has been established!")
-	clientsocket.send(bytes("Welcome!!!", "utf-8"))
-	clientsocket.close()
+
+	msg = "Welcome to the server!"
+	msg = f"{len(msg):<{HEADERSIZE}}" + msg
+
+	clientsocket.send(bytes(msg, "utf-8"))
