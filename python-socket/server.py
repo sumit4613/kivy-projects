0309d46c02c523ba967690eb5229ca7609db0ab1
Sumit Singh <sumiterw11@gmail.com>
2019-04-27 00:02:09 +0530

    major fixes

--- python-socket/server.py
+++ python-socket/server.py
@@ -1,21 +1,24 @@
 """Socket are endpoints that receive data."""
 
 import socket
+import time
+import pickle
 
 HEADERSIZE = 10
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-""" AF_INET corresponds to IPV4
-	SOCK_STREAM corresponds for TCP """
+""" AF_INET corresponds to IPV4, SOCK_STREAM corresponds for TCP """
 
 s.bind((socket.gethostname(), 1234))
 s.listen(5)
 
 while True:
-	clientsocket, address = s.accept()
-	print(f"Connection from {address} has been established!")
+    clientsocket, address = s.accept()
+    print(f"Connection from {address} has been established!")
 
-	msg = "Welcome to the server!"
-	msg = f"{len(msg):<{HEADERSIZE}}" + msg
+    d = {1: "Hey", 2: "There!"}
+    msg = pickle.dumps(d)
 
-	clientsocket.send(bytes(msg, "utf-8"))
+    msg = bytes(f"{len(msg):<{HEADERSIZE}}", "utf-8") + msg
+
+    clientsocket.send(msg, "utf-8")
